9ab2adc054ad6f8be519782a64d6f63ead780319
yjorozco <yjorozco@cnti.gob.ve>
2017-08-09 15:46:07 -0400

    adding feachures

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -30,34 +30,52 @@ public class Percolation {
 	   final int UP = (this.xyTo1D(row-1, col));
 	   final int DOWN = (this.xyTo1D(row+1, col));
 	   final int VALUE = xyTo1D(row, col);
-
+	   
 	   if(grid [row-1][col-1] == 0 && !isFull(row, col)){
 		   grid [row-1][col-1] = OPEN;  
 		   counter++;
 	   }
+	   
+	   if(row -1 == 0){
+		   uf.union(VALUE, 0);
+	       grid [row-1][col-1] = FULL;
+	   }
+	   if(row + 1 == n){
+		   uf.union(VALUE, n*n);
+		   grid [row-1][col-1] = FULL;
+	   }
 	    
-	   if(LEFT > 0 && isOpen(row, col) && !uf.connected(LEFT, VALUE)){
-		   uf.union(LEFT, VALUE);
-	   }
-	   if(RIGHT < n  && isOpen(row, col) && !uf.connected(RIGHT, VALUE)){
-		   uf.union(RIGHT, VALUE);
-	   }
-	   if(UP > 0 && isOpen(row, col) && !uf.connected(UP, VALUE)){
-		   uf.union( UP, VALUE);
-	   }
-	   if(DOWN < n  && isOpen(row, col) && !uf.connected(DOWN, VALUE)){
-		   uf.union(DOWN, VALUE);
-	   }	   
+	   if(LEFT > 0 &&  (col - 1) > 0 && grid [row - 1][col - 2] >0){
+		   uf.union(VALUE, LEFT);
 
-	   int top = 0;
-	   
-	   while(top < n-1){		   
-		   if(isOpen(row, col) && uf.connected( top , VALUE))			   
-			   grid [row-1][col-1] = 2;
-		   top++;
-		   
 	   }
+	   if(RIGHT < (n * n) && col <  n && grid [row - 1][col] >0){
+		   uf.union(VALUE, RIGHT);
+
+	   }
+	   if(UP > 0 && (row - 1) > 0 && grid [row - 2][col - 1] >0){
+		   uf.union(VALUE, UP);
+
+	   }
+
+	   if(DOWN < (n * n) && (row + 1) < n && grid [row + 2][col - 1] >0){
+		   uf.union(VALUE, DOWN);
+
+	   }   
 	   
+
+	   for(int y=0; y < n;  y++){
+		   for(int x=0; x < n; x++){
+			   if(xyTo1D(y, x +1) >= 0 && y - 1 > 0 && uf.connected(xyTo1D(y + 1, x + 1), xyTo1D(y, x +1)) && grid [y - 1][x] == FULL &&  grid [y][x] == OPEN)
+				   grid [y][x] = FULL;
+			   if(xyTo1D(y + 2, x) < n * n && y + 1  < n && uf.connected(xyTo1D(y + 1, x + 1), xyTo1D(y + 2, x)) && grid [y + 1][x] == FULL &&  grid [y][x] == OPEN )
+				   grid [y][x] = FULL;
+			   if(xyTo1D(y + 1, x + 2) < n * n && x + 1 < n && uf.connected(xyTo1D(y + 1, x + 1), xyTo1D(y + 1, x + 2)) && grid [y][x + 1] == FULL &&  grid [y][x] == OPEN)
+				   grid [y][x] = FULL;
+			   if(xyTo1D(y + 1, x) >= 0 && x - 1 > 0 && uf.connected(xyTo1D(y + 1 , x + 1), xyTo1D(y + 1, x)) && grid [y][x - 1] == FULL &&  grid [y][x] == OPEN)
+				   grid [y][x] = FULL;
+		   }
+	   }
 
 	   
    }
@@ -69,6 +87,7 @@ public class Percolation {
    }
    
    public boolean isFull(int row, int col){	  
+
 	   if(grid [row-1][col-1] == FULL)
 		   return true;
 	   return false;	   
@@ -83,7 +102,7 @@ public class Percolation {
 	   return (counter/(n*n) > 0.5);
    }  
    private int xyTo1D(int row, int col){
-	   return (col + row * n);
+	   return ((col-1) + (row-1) * n);
    }
    
    public static void main(String[] args) {
