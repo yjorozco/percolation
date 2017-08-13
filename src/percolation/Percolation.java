206784b6a6dd3a6f75249bb636c089eeed0484bf
yjorozco <yamiloro@gmail.com>
2017-08-12 23:13:27 -0430

    adding new feachures

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -13,8 +13,8 @@ public class Percolation {
    public Percolation(int n) {
 	   this.n = n;
 
-	   grid = new int[n+1][n+1];
-	   uf = new QuickUnionUF((n+1) * (n+1));
+	   grid = new int[n][n];
+	   uf = new QuickUnionUF(n * n + 2);
 	   for(int y=0; y < n;  y++){
 		   for(int x=0; x < n; x++){
 			   grid [y][x] = BLOCK;   
@@ -25,54 +25,56 @@ public class Percolation {
    
    public void open(int row, int col) {		   
 
-	   final int LEFT = (this.xyTo1D(row, col) - 1);
-	   final int RIGHT = (this.xyTo1D(row, col) + 1);
-	   final int UP = (this.xyTo1D(row-1, col));
-	   final int DOWN = (this.xyTo1D(row+1, col));
+	   final int LEFT = this.xyTo1D(row, col - 1);
+	   final int RIGHT = this.xyTo1D(row, col + 1);
+	   final int UP = this.xyTo1D(row - 1, col);
+	   final int DOWN = this.xyTo1D(row + 1, col);
 	   final int VALUE = xyTo1D(row, col);
-	   
-	   if(grid [row-1][col-1] == 0 && !isFull(row, col)){
+
+	   if(grid [row-1][col-1] == BLOCK){
 		   grid [row-1][col-1] = OPEN;  
 		   counter++;
 	   }
 	   
-	   if(row -1 == 0){
+	   if(row - 1 == 0){
 		   uf.union(VALUE, 0);
-	       grid [row-1][col-1] = FULL;
 	   }
-	   if(row + 1 == n-1){
-		   uf.union(VALUE, n*n);
-		   grid [row-1][col-1] = FULL;
-	   }
-	    
-	   if(LEFT > 0 &&  (col - 1) > 0 && grid [row - 1][col - 2] >0){
+	   
+	   
+	   if(LEFT > 0 &&  (col - 1) > 0 && grid [row - 1][col - 2] > 0){
 		   uf.union(VALUE, LEFT);
+		   
 	   }
-	   if(RIGHT < (n * n) && col <  n && grid [row - 1][col] >0){
+	   if(RIGHT < (n * n) && col <  n && grid [row - 1][col] > 0){
 		   uf.union(VALUE, RIGHT);
+		   
 	   }
-	   if(UP > 0 && (row - 1) > 0 && grid [row - 2][col - 1] >0){
+	   if(UP > 0 && (row - 1) > 0 && grid [row - 2][col - 1] > 0){
 		   uf.union(VALUE, UP);
+
 	   }
 
-	   if(DOWN < (n * n) && (row + 1) < n && grid [row + 2][col - 1] >0){
+	   if(DOWN < (n * n) && row < n && grid [row][col - 1] > 0){
 		   uf.union(VALUE, DOWN);
+
 	   }   
-	   watchFull();
-	   
+
+
    }
    
    public boolean isOpen(int row, int col) {
 	   if(grid [row-1][col-1] == OPEN)
 		   return true;
 	   return false;
-   }
-   
-   public boolean isFull(int row, int col){	  
+   }   
 
-	   watchFull();
-	   if(grid [row-1][col-1] == FULL)
-		   return true;
+   public boolean isFull(int row, int col){		   
+      if(grid [row-1][col-1] == OPEN && uf.connected(0, xyTo1D(row, col))){
+		   if(row - 1== (n - 1)){
+			   uf.union(xyTo1D(row, col), (n * n - 1));
+		   } 
+		   return true;	
+      }
 	   return false;	   
    }
    
@@ -80,33 +82,17 @@ public class Percolation {
 	   return counter;
    }
    
-   public boolean percolates() {
-	  
-	   return (counter/(n*n) > 0.5);
+   public boolean percolates() {	  
+	   if(uf.connected(0, (n * n - 1))){
+		   return true;
+	   }
+	   return false;
    }  
    private int xyTo1D(int row, int col){
-	   return ((col-1) + (row-1) * n);
+	   return ((col - 1) + (row - 1) * n);
    }
    
-   private void watchFull(){
-	   for(int y=0; y < n;  y++){
-		   for(int x=0; x < n; x++){
-			   if(xyTo1D(y, x +1) >= 0 && y - 1 > 0 && uf.connected(xyTo1D(y + 1, x + 1), xyTo1D(y, x +1)) && grid [y - 1][x] == FULL &&  grid [y][x] == OPEN){
-				   grid [y][x] = FULL;
-			   }			   
-			   if(xyTo1D(y + 2, x) < n * n && y + 1  < n && uf.connected(xyTo1D(y + 1, x + 1), xyTo1D(y + 2, x)) && grid [y + 1][x] == FULL &&  grid [y][x] == OPEN){
-				   grid [y][x] = FULL;
-			   }
-			   if(xyTo1D(y + 1, x + 2) < n * n && x + 1 < n && uf.connected(xyTo1D(y + 1, x + 1), xyTo1D(y + 1, x + 2)) && grid [y][x + 1] == FULL &&  grid [y][x] == OPEN){
-				   grid [y][x] = FULL;
-			   }
-			   if(xyTo1D(y + 1, x) >= 0 && x - 1 > 0 && uf.connected(xyTo1D(y + 1 , x + 1), xyTo1D(y + 1, x)) && grid [y][x - 1] == FULL &&  grid [y][x] == OPEN){
-				   grid [y][x] = FULL; 
-			   }				  
-		   }
-	   }
-   }
-   
+  
    public static void main(String[] args) {
 	   
    }  // test client (optional)
